42d29f30295b42b0037df1c4086030099c935408
Glebuska <gleb.mirzazyanov@gmail.com>
2020-09-24 10:13:28 +0300

    add result

--- src/graph.py
+++ src/graph.py
@@ -1,8 +1,29 @@
 from pygraphblas import *
 from pyformlang.regular_expression import Regex
 
-from src.rpq import rpq
 
+def rpq(g, r):
+    kron = g.get_intersection(r)
+
+    closure_matrix = Matrix.sparse(BOOL, kron.size, kron.size)
+    for label in kron.matrices:
+        if kron.matrices[label].nrows < kron.size:
+            kron.matrices[label].resize(kron.size, kron.size)
+        closure_matrix += kron.matrices[label]
+
+    tmp = closure_matrix.dup()
+    closure_matrix += closure_matrix @ closure_matrix
+    while not tmp.iseq(closure_matrix):
+        tmp = closure_matrix
+        closure_matrix += closure_matrix @ closure_matrix
+
+    res = Matrix.sparse(BOOL, g.size, g.size)
+    for i, j, _ in zip(*closure_matrix.select(lib.GxB_NONZERO).to_lists()):
+        i_g, i_r = i // r.size, i % r.size
+        j_g, j_r = j // r.size, j % r.size
+        res[i_g, j_g] = True
+
+    return kron, res
 
 class Graph:
     def __init__(self):
@@ -119,3 +140,4 @@ class Graph:
 
         for state in dfa._final_states:
             self.final_states.append(states[state])
+            
